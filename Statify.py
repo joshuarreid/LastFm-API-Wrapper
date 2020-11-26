00d125411a25a4aeb9406cdc8faa64de84ce8330
Joshua Reid <60335156+joshuarreid@users.noreply.github.com>
2020-11-26 15:46:41 -0500

    Update Statify.py

--- Statify.py
+++ Statify.py
@@ -1,41 +1,85 @@
 import pylast
-from Token import lastfm_network, lastfm_username
+from Token import lastfm_network
 import Timestamp
 
-###Changing lastfm_username here allows to lookup any account###
-#LASTFM = lastfm_network.get_user(lastfm_username)
 
+### Converts playback time from UTC time zone to EST time zone ####
+def playbackTimeUtcToEst(utcTime):
+    hourMin = str(utcTime)[13:]
+    return Timestamp.utcToEst(hourMin)
 
 ### Grabs played tracks within a time interval ###
-def getTracks(time_from, time_to, user):
-    tracks = lastfm_network.get_user(user).get_recent_tracks(cacheable=False, limit=None, time_from=time_from, time_to=time_to)
-    return tracks
-
+def getTracksTimeInterval(timeFrom, timeTo, user):
+    trackList = lastfm_network.get_user(user).get_recent_tracks(cacheable=False, limit=None, time_from=timeFrom, time_to=timeTo)
+    return trackList
 
 ### Grabs played tracks from last 24 hours ###
-def lastDayTracks(user):
-    lastDayTracksList = getTracks(int(Timestamp.twentyFourHours()["time_from"]), int(
+def playbackPastDay(user):
+    pastDayTrackList = getTracksTimeInterval(int(Timestamp.twentyFourHours()["time_from"]), int(
         Timestamp.twentyFourHours()["time_to"]), user)
-    return lastDayTracksList
+    return pastDayTrackList
 
-def pastThreeHoursTracks(user):
-    pastThreeHoursList = getTracks(int(Timestamp.pastThreeHours()["time_from"]), int(
+def playbackPastThreeHours(user):
+    pastThreeHoursTrackList = getTracksTimeInterval(int(Timestamp.pastThreeHours()["time_from"]), int(
         Timestamp.pastThreeHours()["time_to"]), user)
-    return pastThreeHoursList
+    return pastThreeHoursTrackList
 
 ### Grabs played tracks from a year ago ###
 def oneYearAgoTracks(user):
-    oneYearAgoTracksList = getTracks(int(Timestamp.threeSixFive()["time_from"]), int(
+    oneYearAgoTracksList = getTracksTimeInterval(int(Timestamp.threeSixFive()["time_from"]), int(
         Timestamp.threeSixFive()["time_to"]), user)
     return oneYearAgoTracksList
 
-def nowPlaying(user):
+def getNowPlaying(user):
     now_playing = [lastfm_network.get_user(user).get_now_playing()]
     return now_playing
 
-def topTracks(user):
-    topTracksList = lastfm_network.get_user(user).get_top_tracks(period="PERIOD_OVERALL",limit=10)
+def getTopTracks(user, periodInput):
+        topTracksList = lastfm_network.get_user(user).get_top_tracks(period = periodInput, limit=25)
+        return topTracksList
+
+
+def getTopArtist(user, periodInput):
+    topTracksList = lastfm_network.get_user(user).get_top_artists(period=periodInput, limit=25)
     return topTracksList
+
+
+def playCount(user):
+    playCount = lastfm_network.get_user(user).get_playcount()
+    return '{:,}'.format(playCount) ### formats number with comma
+
+def compareUsersTopTracks(user, otherUser, periodInput):
+    topTracksListUser = lastfm_network.get_user(user).get_top_tracks(period = periodInput, limit = 150)
+    topTracksListOtherUser = lastfm_network.get_user(otherUser).get_top_tracks(period=periodInput, limit=150)
+
+    formattedTrackListUser = []
+    for track in topTracksListUser:
+        formattedTrackListUser.append(track.item.title)
+
+    formattedTrackListOtherUser = []
+    for track in topTracksListOtherUser:
+        formattedTrackListOtherUser.append(track.item.title)
+
+    commonTopTracksList = list(set.intersection(set(formattedTrackListUser), set(formattedTrackListOtherUser)))
+    return commonTopTracksList
+
+
+def compareUsersTopArtists(user, otherUser, periodInput):
+    topArtistsListUser = lastfm_network.get_user(user).get_top_artists(period=periodInput, limit = 100)
+    topArtistsListOtherUser = lastfm_network.get_user(otherUser).get_top_artists(period=periodInput, limit= 100)
+
+    formattedArtistListUser = []
+    for artist in topArtistsListUser:
+        formattedArtistListUser.append(artist.item)
+
+    formattedArtistListOtherUser = []
+    for artist in topArtistsListOtherUser:
+        formattedArtistListOtherUser.append(artist.item)
+
+    commonTopArtistsList = list(set.intersection(set(formattedArtistListUser), set(formattedArtistListOtherUser)))
+    return commonTopArtistsList
+
+
 
 
 
